485e6a258055434a7d2af4eb65a618eb932b0746
CliffCrerar <cliff.crerar@gmail.com>
2023-09-07 14:16:50 +0200

    whahha

--- odyssey-lift-off-part1/client/src/index.tsx
+++ odyssey-lift-off-part1/client/src/index.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+const uri = 'http://localhost:4000';
+
+const client = new ApolloClient({
+  uri,
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <GlobalStyles />
+      <Pages />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
